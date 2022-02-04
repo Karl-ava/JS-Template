8f70677722e742c6976fddf8350530da58bfe071
Karl Rosengren <92572905+Karl-ava@users.noreply.github.com>
2022-02-04 11:16:38 +0100

    Bubble sort

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-const myString = "Hello, world!";
-console.log(myString);
+function randomNumberGenerator(min, max){
+    return Math.floor(Math.random()*(max-min)+min)
+}
+
+function randomArrayGenerator(length, min, max){
+    let arr = []
+    for(i=1; i<=length; i++){
+        let randomNumber = randomNumberGenerator(min, max) 
+        arr.push(randomNumber)
+    }
+    return arr
+}
+
+function bubbleSort(array){
+    while(!(sorted(array))){
+        for(i=0; i<array.length; i++){
+            if(array[i]>array[i+1]){
+                let firstElement = array[i], secondElement = array[i+1]
+                array[i] = secondElement
+                array[i+1] = firstElement
+            }
+        }
+    }
+    return array
+}
+
+function sorted(array){
+    for(i=0; i<array.length; i++){
+        if(array[i] > array[i+1]){
+            return false
+        }
+    } 
+    return true
+}
+
+let randomArray = randomArrayGenerator(20, -100, 100)
+console.log(randomArray)
+let sortedArray = bubbleSort(randomArray)
+console.log(sortedArray)
