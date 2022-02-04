3b8203dd8157c83cec8c8abc530a5d4ff33af577
Goby56 <92572905+Karl-ava@users.noreply.github.com>
2022-02-05 00:57:56 +0100

    start to quicksort

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-function randomNumberGenerator(min, max){
-    return Math.floor(Math.random()*(max-min)+min)
+function randomNumberGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomArrayGenerator(length, min, max){
-    let arr = []
-    for(i=1; i<=length; i++){
-        let randomNumber = randomNumberGenerator(min, max) 
-        arr.push(randomNumber)
+function randomArrayGenerator(length, min, max) {
+    let arr = [];
+    for (i = 1; i <= length; i++) {
+        let randomNumber = randomNumberGenerator(min, max);
+        arr.push(randomNumber);
     }
-    return arr
+    return arr;
 }
 
-function bubbleSort(array){
-    while(!(sorted(array))){
-        for(i=0; i<array.length; i++){
-            if(array[i]>array[i+1]){
-                let firstElement = array[i], secondElement = array[i+1]
-                array[i] = secondElement
-                array[i+1] = firstElement
+function bubbleSort(array) {
+    while (!sorted(array)) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                array.swap(i, i + 1);
             }
         }
     }
-    return array
+    return array;
 }
 
-function sorted(array){
-    for(i=0; i<array.length; i++){
-        if(array[i] > array[i+1]){
-            return false
+function quickSort(array, pivotIndex){
+    pivot = array[pivotIndex]
+    array.swap(pivotIndex, -1)
+    for (i = 0; i<array.length; i++){
+        if (array[i] < pivot){
+
         }
-    } 
-    return true
+    }
 }
 
-let randomArray = randomArrayGenerator(20, -100, 100)
-console.log(randomArray)
-let sortedArray = bubbleSort(randomArray)
-console.log(sortedArray)
+function partition()
+
+function sorted(array) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Array.prototype.swap = function (i, j) {
+    [this[i], this[j]] = [this[j], this[i]];
+    return this;
+};
+
+let randomArray = randomArrayGenerator(20, -100, 100);
+console.log(randomArray);
+let sortedArray = bubbleSort(randomArray);
+console.log(sortedArray);
