14923fb1ab8fcef6a842d1fbb6d8dca24df9077e
Goby56 <92572905+Karl-ava@users.noreply.github.com>
2022-02-05 19:54:01 +0100

    quick sort done

--- script.js
+++ script.js
@@ -22,17 +22,49 @@ function bubbleSort(array) {
     return array;
 }
 
-function quickSort(array, pivotIndex){
-    pivot = array[pivotIndex]
-    array.swap(pivotIndex, -1)
-    for (i = 0; i<array.length; i++){
-        if (array[i] < pivot){
+function quickSort(array) {
+    quickSortSplit(array, 0, array.length - 1);
+    return array;
+}
 
-        }
+function quickSortSplit(array, low, high) {
+    if (low < high) {
+        let finalIndex = partition(array, low, high);
+
+        quickSortSplit(array, low, finalIndex - 1);
+        quickSortSplit(array, finalIndex + 1, high);
     }
 }
 
-function partition()
+function partition(array, low, high) {
+    let pivotIndex = medianOfThree(array, low, high);
+    let pivot = array[pivotIndex];
+    array.swap(pivotIndex, high);
+
+    let i = low - 1;
+    for (let j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            array.swap(j, i);
+        }
+    }
+    array.swap(i + 1, high);
+    return i + 1;
+}
+
+function medianOfThree(array, low, high) {
+    let a = randomNumberGenerator(low, high);
+    let b = randomNumberGenerator(low, high);
+    let c = randomNumberGenerator(low, high);
+    let three = [array[a], array[b], array[c]];
+    if (sorted(three)) {
+        return b;
+    } else if (sorted(three.swap(0, 1))) {
+        return a;
+    } else {
+        return c;
+    }
+}
 
 function sorted(array) {
     for (i = 0; i < array.length; i++) {
@@ -48,7 +80,21 @@ Array.prototype.swap = function (i, j) {
     return this;
 };
 
-let randomArray = randomArrayGenerator(20, -100, 100);
-console.log(randomArray);
-let sortedArray = bubbleSort(randomArray);
-console.log(sortedArray);
+const { performance } = require("perf_hooks");
+
+function profile(num) {
+    let total = 0;
+    for (i = 0; i < num; i++) {
+        let randomArray = randomArrayGenerator(10, 0, 10);
+
+        let startTime = performance.now();
+
+        quickSort(randomArray);
+
+        let endTime = performance.now();
+        total += endTime - startTime;
+    }
+    return total / num;
+}
+
+console.log(profile(10));
